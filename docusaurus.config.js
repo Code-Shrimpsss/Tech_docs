b6b78a4dde8dd4c6d9499864e5d5aee93d17cfbd
978309462@qq.com <978309462@qq.com>
2023-05-23 08:08:28 +0800

    feature: new page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'My Site',
-	tagline: 'Dinosaurs are cool',
+	title: 'Shrimps Dev-Book',
+	tagline: 'A treasure trove of front-end technology',
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
@@ -133,6 +133,11 @@ const config = {
 				theme: lightCodeTheme,
 				darkTheme: darkCodeTheme,
 			},
+			// docs: {
+			// 	sidebar: {
+			// 		hideable: true,
+			// 	},
+			// },
 		}),
 };
 
